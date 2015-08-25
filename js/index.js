ac920cbef50881984beed5cb6b3b22642c2aaaf6
ttaccp <ttaccp@vip.qq.com>
2015-08-25 13:29:13 +0800

    优化

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// ===============step1========================
 var Msg = (function(window, $){
 	
 	var count = 0;
@@ -35,64 +34,7 @@ hammertime.on("panstart panend", function (ev) {
 	} else {
 		if((x - startX) > 30){
 			$('#step1').hide();
-			$('#step2').show();
+			$('#step2').fadeIn(1000);
 		}
 	}
 });
-//$('#step1').hide();
-//$('#step2').show();
-
-// ===============step2========================
-$('#numlist .num').live('click', function(){
-	var self = $(this),
-		val = self.attr('_val'),
-		num = $('#numbox [_val="' + val + '"]'),
-		topnumVal = num.attr('_val');
-	
-	if(num.length > 0){
-		var pre = num.prev();
-		if(pre.length > 0 && pre.hasClass('cur') && val == topnumVal){
-			num.addClass('cur')
-		} else if(pre.length == 0 && val == topnumVal){
-			num.addClass('cur')
-		} else {
-			clearNumSelected();
-		}
-	} else {
-		clearNumSelected();
-	}
-	
-	if($('#numbox .num:last').hasClass('cur')){
-		console.log('next');
-	}
-});
-
-function clearNumSelected(){
-	
-	$('#numbox .num').removeClass('cur');
-	var count = 0,
-		temp = ['-30px', '30px', '-30px', '30px'];
-		
-	animate();
-
-	function animate(){
-		
-		$('#numbox')
-		.stop()
-		.animate({
-			'margin-left': temp[count]
-		}, 80, function(){
-			if(temp.length > ++count){
-				animate();
-			}
-		});
-	}
-}
-
-
-
-
-
-
-
-
---
+++ js/pwd.js
@@ -0,0 +1,45 @@
+$('#numlist .num').live('click', function(){
+	var self = $(this),
+		val = self.attr('_val'),
+		num = $('#numbox [_val="' + val + '"]'),
+		topnumVal = num.attr('_val');
+	
+	if(num.length > 0){
+		var pre = num.prev();
+		if(pre.length > 0 && pre.hasClass('cur') && val == topnumVal){
+			num.addClass('cur')
+		} else if(pre.length == 0 && val == topnumVal){
+			num.addClass('cur')
+		} else {
+			clearNumSelected();
+		}
+	} else {
+		clearNumSelected();
+	}
+	
+	if($('#numbox .num:last').hasClass('cur')){
+		console.log('next');
+	}
+});
+
+function clearNumSelected(){
+	
+	$('#numbox .num').removeClass('cur');
+	var count = 0,
+		temp = ['-30px', '30px', '-30px', '30px'];
+		
+	animate();
+
+	function animate(){
+		
+		$('#numbox')
+		.stop()
+		.animate({
+			'margin-left': temp[count]
+		}, 80, function(){
+			if(temp.length > ++count){
+				animate();
+			}
+		});
+	}
+}
