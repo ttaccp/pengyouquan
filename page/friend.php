af40228a9aa3d8e57d3f8d8bfa878830df23bf09
liasica <magicrolan@qq.com>
2015-08-27 18:10:12 +0800

    分享从头开始

--- page/friend.php
+++ page/friend.php
@@ -561,7 +561,6 @@ $status = $wechat->getStatusByOpenid($userInfo->openid);
 
   // 设置分享信息
   function setShareInfo() {
-
     var shareData = {
       title: '德勤内部朋友圈大曝光 | 2016校园招聘精彩开幕！', //分享标题
       desc: '德勤内部闹翻天啦~还不快来看看！',
