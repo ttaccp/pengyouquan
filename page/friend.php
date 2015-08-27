0b089d1d915c0bb0783ff5aed8d36664dc532178
liasica <magicrolan@qq.com>
2015-08-27 18:09:10 +0800

    分享从头开始

--- page/friend.php
+++ page/friend.php
@@ -565,7 +565,7 @@ $status = $wechat->getStatusByOpenid($userInfo->openid);
     var shareData = {
       title: '德勤内部朋友圈大曝光 | 2016校园招聘精彩开幕！', //分享标题
       desc: '德勤内部闹翻天啦~还不快来看看！',
-      link: location.href,
+      link: "http://third.liasica.com/pengyouquan/page",
       //    imgUrl: 'imgurl',
       success: function () {
         location.href = 'http://deloitte.careerfrog.com.cn';
