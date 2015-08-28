5e3454cc50a328a9770cb5c02b443c48190e4c87
ttaccp <ttaccp@vip.qq.com>
2015-08-28 17:54:32 +0800

    ..

--- page/friend.php
+++ page/friend.php
@@ -470,7 +470,7 @@ $status = $wechat->getStatusByOpenid($userInfo->openid);
 
 <div class="tip-txt">朋友圈就看到这啦！想了解更多关于德勤大趴信息，锁定8月31日空中宣讲，快来预报名吧！</div>
 <br />
-<div class="tip-txt">宣讲预报名地址：<a href="http://deloitte.careerfrog.com.cn" class="a_address">deloitte.careerfrog.com.cn</a></div>
+<div class="tip-txt">宣讲预报名地址：deloitte.careerfrog.com.cn</div>
 <input type="button" value="点击分享并预约" class="share-btn" onclick="Share.show();"/>
 
 <div class="qrcode" id="qrcode">
